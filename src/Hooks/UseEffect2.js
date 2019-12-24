2aa10db8f03c58b678ab849f8d47597b4ce95cbc
Ahad <ahad3481@gmail.com>
2019-12-24 17:49:45 +0500

    added some changes

--- src/Hooks/UseEffect2.js
+++ src/Hooks/UseEffect2.js
@@ -3,19 +3,25 @@ import React,{useState,useEffect} from 'react'
 function UseEffect2() {
     const[x,setX]=useState(0);
     const[y,setY]=useState(0);
-
+    const[display,setDisplay]=useState(true)
     const logMousePosition=(e)=>{
         console.log('Mouse Move');
         setX(e.clientX);
         setY(e.clientY)
     }
     useEffect(()=>{
+        //component did mount and component did update
         console.log('useEffect called');
         window.addEventListener('mousemove',logMousePosition)
+        //component will Unmount
+        return(
+          window.removeEventListener('mousemove',logMousePosition)
+        )
     },[])
   return (
     <div>
-      X:{x} Y:{y}
+      <button onClick={()=>setDisplay(false)}>Toogle display</button>
+      {display && <p>X:{x} Y:{y}</p> }
     </div>
   )
 }
