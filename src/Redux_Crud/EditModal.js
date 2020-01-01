4c17a8400a98a56b56df7507fbf59aa929a926d4
Ahad <ahad3481@gmail.com>
2020-01-01 16:50:58 +0500

    Some Modifications

---
+++ src/Redux_Crud/EditModal.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import { Modal, Button } from 'react-bootstrap'
+import { show } from 'react-bootstrap'
+import {connect} from 'react-redux'
+import { Edit } from '../Redux/User/UserActions'
+
+class EditModal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      id: '',
+      name: '',
+      email: '',
+      phone: ''
+    }
+  }
+  componentDidMount() {
+    const { name, email, phone } = this.state
+    const { selectedUser } = this.props
+    this.setState({
+      id: selectedUser.id,
+      name: selectedUser.name,
+      email: selectedUser.email,
+      phone: selectedUser.phone
+    })
+  }
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+  handleSave = () => {
+    const{closeModal,users,editUser}=this.props
+    const user = this.state
+    const index = users.findIndex(prevUser => prevUser.id === user.id)
+    editUser(user,index)
+    closeModal()
+ 
+  }
+
+  render() {
+    const { closeModal } = this.props
+    const { name, email, phone } = this.state
+    return (
+      <div>
+        <Modal show="true" >
+          <Modal.Header closeButton>
+            <Modal.Title>Edit User</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+
+              <div class="form-group">
+                <label >Name:</label>
+                <input value={name} name="name" onChange={this.changeHandler} class="form-control" />
+              </div>
+              <div class="form-group">
+                <label >Email:</label>
+                <input type="text" class="form-control" value={email} name="email" onChange={this.changeHandler} />
+              </div>
+              < div class="form-group">
+                <label >Phone:</label>
+                <input type="text" class="form-control" value={phone} name="phone" onChange={this.changeHandler} />
+              </div>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              Close
+          </Button>
+            <Button variant="primary" onClick={this.handleSave} >
+              Save Changes
+          </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch=>{
+  return{
+    editUser:(user,index)=>dispatch(Edit(user,index))
+  }
+}
+export default connect(null,mapDispatchToProps) (EditModal)
+
---
+++ src/Redux_Crud/MainContainer.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import { fetchUser } from '../Redux/User/UserActions'
+import DisplayUsers from './DisplayUsers'
+import AddUser from './AddUser'
+import EditModal from './EditModal'
+
+
+function MainContainer({ fetchUser, userData }) {
+  const [isAddUser, setIsAddUser] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [selectedUser, setSelectedUser] = useState({})
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  const addUserForm = () => setIsAddUser(true)
+  const closeUserForm = () => setIsAddUser(false)
+
+  const openModal = (user) => {
+    setShowModal(true)
+    setSelectedUser(user)
+  }
+  const closeModal=()=>{
+    setShowModal(false)
+  }
+  
+  return (
+    <>
+      {userData.error && <h2>Error</h2>}
+      {userData.users && !isAddUser && <DisplayUsers users={userData.users} openModal={openModal}/>}
+      {!isAddUser && <button className="btn btn-primary  ml-3 mb-3" onClick={addUserForm}>Add User</button>}
+      {isAddUser && <AddUser closeUserForm={closeUserForm}  />}
+      {!isAddUser && showModal && < EditModal  closeModal={closeModal} selectedUser={selectedUser} users={userData.users} />}
+    </>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    userData: state.user
+  }
+
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUser: () => dispatch(fetchUser())
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer)
